fe5dcc9ce0f172e2f86c24a7b0a27321d29b2470
Kentaro Yamada <Kentaro Yamada>
2016-08-29 02:14:16 +0900

    d

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    '/project'
+  end
+
+  def after_inactive_sign_in_path_for(resource)
+    '/project'
+  end
+end
