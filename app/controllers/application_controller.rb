926ef2a70b1620faf6ffc096b906db96dcda7225
Kentaro Yamada <Kentaro Yamada>
2016-08-29 01:51:28 +0900

    devise2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    '/project/email'
+    '/project'
   end
 
   def after_inactive_sign_up_path_for(resource)
